be99762f41bb1d248f8ce064bcf08f31bc3c5976
Marwan Zaky <marouaneezzaky@gmail.com>
2023-01-20 02:04:11 +0100

    Create python_comments.py

---
+++ python_comments.py
@@ -0,0 +1,19 @@
+# 1. Creating a Comment
+# This is a comment
+print('Hello, World!')
+
+print('Hello, World!')  # This is a comment
+# print('Hello, World!')
+
+# 2. Multiline Comments
+# This is a comment
+# Written in
+# More than just one line
+print('Hello, World!')
+
+"""
+    This is a comment
+    Written in
+    More than just one line
+"""
+print("Hello, World!")
