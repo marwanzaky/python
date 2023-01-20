0bb580c7947fb5e1a01632a5d6b3483ad9d3282f
Marwan Zaky <marouaneezzaky@gmail.com>
2023-01-20 02:14:24 +0100

    Create python_variables.py

---
+++ python_variables.py
@@ -0,0 +1,33 @@
+# Creating variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# Change variable type
+x = "Smith"
+print(x)
+
+# Casting
+x = str(3)      # x will be '3'
+y = int(3)      # y will be 3
+z = float(3)    # z will be 3.0
+print(x)
+print(y)
+print(z)
+
+# Get the Type
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+# Single or Double Quotes?
+x = "John"
+# is the same as
+x = 'John'
+
+# Case-Sensitive
+a = 4
+A = "Sally"
+# A will not overwrite a
